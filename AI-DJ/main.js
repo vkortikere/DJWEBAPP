0d3cb684f3000a02798accb6efdabccdc90e937a
vkortikere <69329819+vkortikere@users.noreply.github.com>
2021-06-03 20:20:20 -0400

    Add files via upload

---
+++ AI-DJ/main.js
@@ -0,0 +1,56 @@
+song=""
+leftWristX = 0;
+leftWristY= 0;
+rightWristX = 0;
+rightWristY= 0;
+
+function preload(){
+song=loadSound("music.mp3");
+}
+
+ function setup(){
+ canvas = createCanvas(600,500);
+canvas.center();
+    
+video = createCapture(VIDEO);
+video.hide();
+poseNet = ml5.poseNet(video, modelLoaded);
+poseNet.on('pose', gotPoses);
+}
+function modelLoaded(){
+    console.log('PoseNet is initialized')
+}
+
+function draw(){
+image(video, 0, 0, 600, 500);
+fill("#FF0000");
+stroke("#FF0000");
+circle(leftWristX,leftWristY, 20);
+inNumberLeftWristY = Number(leftWristY);
+remove_decimals = floor(inNumberLeftWristY);
+volume= remove_decimals/500;
+document.getElementById("volume").innerHTML ="Volume = "+volume;
+song.setVolume(volume);
+}
+function play(){
+    song.play();
+    song.setVolume(0.5,10);
+    song.rate(2);
+}
+function pause(){
+    song.pause();
+}
+function stop(){
+    song.stop();
+}
+function gotPoses(results){
+    if(results.length >0){
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX = " + leftWristX+" leftWristY = "+leftWristY)
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX = " + rightWristX+" rightWristY = "+rightWristY)
+    }
+}
